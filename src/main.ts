33519e1fb880cb9826fc56a5d1b1a05ef9bbd0d4
Johan Ledoux <johan.ledoux25@gmail.com>
2025-03-19 00:42:32 +0100

    + langs

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Swagger setup
+  app.enableCors({
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Ecodeli.fr API')
     .setDescription('Documentation de l’API de Ecodeli.fr')
