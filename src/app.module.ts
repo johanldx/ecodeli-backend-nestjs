86d73a76da0cf276ff48a46486c29244c2ba570d
Johan Ledoux <johan.ledoux25@gmail.com>
2025-03-22 02:08:29 +0100

    + intégration bdd, modification fichier de traductions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { I18nModule } from './i18n/i18n.module';
-import { ConfigModule } from '@nestjs/config';
 import { EmailModule } from './email/email.module';
 
 @Module({
   imports: [
-    I18nModule,
     ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT') || '5432', 10),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        synchronize: true, // ⚠️ À désactiver en production
+      }),
+    }),
+    I18nModule,
     EmailModule,
   ],
   controllers: [AppController],
