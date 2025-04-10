bf72b95142c7bf9b2b38b4bb7d5ca3bba109815c
Guyhomekon <arouquinerie@gmail.com>
2025-04-11 00:52:51 +0200

    EDIT - Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,19 +3,16 @@ set -e
 
 cd /runner
 
-# Chargement optionnel d'un .env monté via volume Docker
 if [ -f "/runner/env/.env" ]; then
   echo "[INFO] Chargement du fichier .env..."
   export $(grep -v '^#' /runner/env/.env | xargs)
 fi
 
-# Vérifie que le GITHUB_TOKEN est bien présent
 if [ -z "$GITHUB_TOKEN" ]; then
   echo "[ERREUR] GITHUB_TOKEN non défini. Abandon."
   exit 1
 fi
 
-# Configure le runner s'il ne l'est pas déjà
 if [ ! -f ".runner" ]; then
   echo "[INFO] Configuration du GitHub Actions runner..."
   ./config.sh --unattended \
@@ -28,5 +25,4 @@ else
   echo "[INFO] Le runner est déjà configuré. Démarrage..."
 fi
 
-# Lance le runner
 exec ./run.sh
