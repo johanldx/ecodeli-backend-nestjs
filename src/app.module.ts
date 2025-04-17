bf4ff4d0b3fe3cacfdc17a597cb8b266021a0ea3
Rayan <rayanchebicheb@yahoo.fr>
2025-04-18 01:02:08 +0200

    traduction admin/user

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,6 +23,7 @@ import { SubscriptionPaymentsModule } from './subscription-payments/subscription
 import { ReleaseCartAdsModule } from './release-cart-ads/release-cart-ads.module';
 import { ShoppingAdsModule } from './shopping-ads/shopping-ads.module';
 import { ProvidersModule } from './providers/providers.module';
+import { RoutesModule } from './routes/routes.module';
 
 @Module({
   imports: [
@@ -58,7 +59,8 @@ import { ProvidersModule } from './providers/providers.module';
     SubscriptionPaymentsModule,
     ReleaseCartAdsModule,
     ShoppingAdsModule,
-    ProvidersModule, // ✅ ajouté ici dans les imports
+    ProvidersModule,
+    RoutesModule, // ✅ ajouté ici dans les imports
   ],
   controllers: [AppController],
   providers: [
