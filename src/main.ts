7ce5e0bbc57d0ec814ff97e32a8adf2432f389c3
Johan Ledoux <johan.ledoux25@gmail.com>
2025-06-29 23:13:21 +0200

    correctif 1

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,11 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:3001',
+      'http://192.168.0.179:3001',
+      'https://ecodeli.fr',
+    ],
     methods: 'GET,POST,PUT,DELETE, PATCH',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
